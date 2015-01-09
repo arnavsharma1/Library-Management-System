bf315099b9a0e41ad32b7cf99e8b87d28aae1a6a
arnavsharma1 <arnav.sharma1@utdallas.edu>
2015-01-09 15:09:29 -0600

    Create Borrower.java

---
+++ Borrower.java
@@ -0,0 +1,19 @@
+package library_mgmt_system;
+
+public class Borrower {
+	String FName;
+	String Address;
+	String Phone;
+	String LName;
+	int CardNo;
+
+	public Borrower(String fName, String address, String phone, String lName,
+			int cardNo) {
+		FName = fName;
+		Address = address;
+		Phone = phone;
+		LName = lName;
+		CardNo = cardNo;
+	}
+
+}
